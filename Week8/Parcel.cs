28c447e3f18a1581d1b05b2445899520ac17a864
Yarr2 <yarema.stus@gmail.com>
2026-03-24 18:35:23 +0200

    Added Parcel.cs with overriden PrintInfo and CalculateCost methods

---
+++ Week8/Parcel.cs
@@ -0,0 +1,24 @@
+﻿namespace Week8;
+
+public class Parcel : DeliveryItem
+{
+    public string Dimensions { get; }
+    private const int StartingPrice = 50;
+    private const int PriceForKilogram = 25;
+
+    public Parcel(string trackingNumber, int weight, string dimensions) : base(trackingNumber, weight)
+    {
+        Dimensions = dimensions;
+    }
+
+    public override double CalculateCost()
+    {
+        return StartingPrice + Weight * PriceForKilogram;
+    }
+
+    public override void PrintInfo()
+    {
+        base.PrintInfo();
+        Console.WriteLine($"Dimensions - {Dimensions}");
+    }
+}
