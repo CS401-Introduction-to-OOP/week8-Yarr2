ffe09001a64d52b5ae26c11cca8c7223ea5096b6
Yarr2 <yarema.stus@gmail.com>
2026-03-24 18:40:11 +0200

    Added CargoContainer.cs

---
+++ Week8/CargoContainer.cs
@@ -0,0 +1,22 @@
+﻿namespace Week8;
+
+public class CargoContainer<T> where T : DeliveryItem
+{
+    private List<T> _items = new List<T>();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+
+    public double GetTotalCost()
+    {
+        double result = 0;
+        foreach (T item in _items)
+        {
+            result += item.CalculateCost();
+        }
+
+        return result;
+    }
+}
