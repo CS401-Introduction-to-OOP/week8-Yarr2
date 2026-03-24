2a64cb91e5d53e28a9ddc56561dfa94f4529cc4e
Yarr2 <yarema.stus@gmail.com>
2026-03-24 18:29:10 +0200

    Added Letter.cs with overriden CalculateCost method

---
+++ Week8/Letter.cs
@@ -0,0 +1,14 @@
+﻿namespace Week8;
+
+public class Letter : DeliveryItem
+{
+    private const int StartingPrice = 15;
+    private const int AddedValueForKilogram = 10;
+    public Letter(string trackingNumber, int weight) : base(trackingNumber, weight)
+    { }
+
+    public override double CalculateCost()
+    {
+        return StartingPrice + Weight * AddedValueForKilogram;
+    }
+}
