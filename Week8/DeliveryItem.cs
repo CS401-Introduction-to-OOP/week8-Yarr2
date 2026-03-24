e86a0a772c02ede1131483bb4198b86ef4d07ef5
Yarr2 <yarema.stus@gmail.com>
2026-03-24 18:25:27 +0200

    Added DeliveryItem.cs

---
+++ Week8/DeliveryItem.cs
@@ -0,0 +1,20 @@
+﻿namespace Week8;
+
+public abstract class DeliveryItem
+{
+    public string TrackingNumber;
+    public int Weight;// weight in kilograms
+
+    public DeliveryItem(string trackingNumber, int weight)
+    {
+        TrackingNumber = trackingNumber;
+        Weight = weight;
+    }
+    
+    public abstract double CalculateCost();
+
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine($"Package with tracking number - {TrackingNumber} and weight - {Weight}");
+    }
+}
